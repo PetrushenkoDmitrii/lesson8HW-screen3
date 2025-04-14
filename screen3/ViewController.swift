33adaddc2a10eac1c1867f309308acfb32d6066c
PetrushenkoDmitry <petrushenkodmitrii@yandex.ru>
2025-04-14 12:07:24 +0300

    +

--- screen3/ViewController.swift
+++ screen3/ViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var userName: UITextField!
+    
+    @IBOutlet weak var password: UITextField!
+    
+    @IBOutlet weak var confirmPassword: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func printChanges(_ sender: Any) {
+        if userName.text == "" || password.text == "" || confirmPassword.text == "" {
+            print ("Please fill all fields")
+        } else {
+            print("User name: \(userName.text!)")
+            print("Password: \(password.text!)")
+            print("Confirm Password name: \(confirmPassword.text!)")
+        }
+    }
 }
-
